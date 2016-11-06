5731925eaf668ef42445974d9189496dfd3dba6f
Nikita Ponomarev <ponom.nikita@gmail.com>
2016-11-06 12:44:38 +0300

    CodeStyle

--- code/ponomarev-nikita/ponomarev-nikita-Model/src/main/java/ru/agile/SquareConverter/Model/FromSqrMeter.java
+++ code/ponomarev-nikita/ponomarev-nikita-Model/src/main/java/ru/agile/SquareConverter/Model/FromSqrMeter.java
@@ -2,16 +2,16 @@ package ru.agile.SquareConverter.Model;
 
 public class FromSqrMeter {
 
-    private final static double TOKILOMETERMULTIPLIER = 0.000001;
-    private final static double TOHECTAREMULTIPLIER = 0.0001;
-    private final static double TOARMULTIPLIER = 0.01;
-    private final static double TOSQRSANTIMETERMULTIPLIER = 10000;
-    private final static double TOSQRMILLIMETERMULTIPLIER = 1000000;
-    private final static double TOSQRYARDMULTIPLIER = 1.196;
-    private final static double TOSQRFOOTMULTIPLIER = 10.76;
-    private final static double TOSQRINCHMULTIPLIER = 1550;
-    private final static double TOSQRACREMULTIPLIER = 0.0002471;
-    private final static double TOSQRMILEMULTIPLIER = 0.0000003861;
+    private static final double TOKILOMETERMULTIPLIER = 0.000001;
+    private static final double TOHECTAREMULTIPLIER = 0.0001;
+    private static final double TOARMULTIPLIER = 0.01;
+    private static final double TOSQRSANTIMETERMULTIPLIER = 10000;
+    private static final double TOSQRMILLIMETERMULTIPLIER = 1000000;
+    private static final double TOSQRYARDMULTIPLIER = 1.196;
+    private static final double TOSQRFOOTMULTIPLIER = 10.76;
+    private static final double TOSQRINCHMULTIPLIER = 1550;
+    private static final double TOSQRACREMULTIPLIER = 0.0002471;
+    private static final double TOSQRMILEMULTIPLIER = 0.0000003861;
 
     public double toSqrKilometer(final double sqrMeterCount) {
         if (sqrMeterCount < 0) {
