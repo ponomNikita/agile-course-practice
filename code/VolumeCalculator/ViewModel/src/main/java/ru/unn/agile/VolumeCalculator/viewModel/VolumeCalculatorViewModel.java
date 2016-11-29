c0a0bf8c8c2d7702ec124f018f2cb836788d74f3
Пономарев Никита <ponom.nikita@gmaol.com>
2016-11-29 21:41:35 +0300

    Implementation view and viewModel

--- code/VolumeCalculator/ViewModel/src/main/java/ru/unn/agile/VolumeCalculator/viewModel/VolumeCalculatorViewModel.java
+++ code/VolumeCalculator/ViewModel/src/main/java/ru/unn/agile/VolumeCalculator/viewModel/VolumeCalculatorViewModel.java
@@ -1,16 +1,159 @@
 package ru.unn.agile.VolumeCalculator.viewModel;
 
-/**
- * Created by ponom on 24.11.2016.
- */
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class VolumeCalculatorViewModel {
-    private boolean isCalculateButtonDisabled;
+
+    private final BooleanProperty isCalculateDisable = new SimpleBooleanProperty(true);
+    private final BooleanProperty isParam1Visible = new SimpleBooleanProperty(false);
+    private final BooleanProperty isParam2Visible = new SimpleBooleanProperty(false);
+    private final StringProperty resultVolume = new SimpleStringProperty("");
+    private final StringProperty param1Name = new SimpleStringProperty("Param1");
+    private final StringProperty param2Name = new SimpleStringProperty("Param2");
+
+    private final ObjectProperty<EVolumeTypes> selectedVolumeItem = new SimpleObjectProperty<>();
+    private final ObjectProperty<ObservableList<EVolumeTypes>> volumeTypeListItems =
+            new SimpleObjectProperty<>(FXCollections.observableArrayList(EVolumeTypes.values()));
+
+    //private final SelectedVolumeItemChangeListener listener = new SelectedVolumeItemChangeListener();
 
     public VolumeCalculatorViewModel() {
-        isCalculateButtonDisabled = true;
+
+        selectedVolumeItem.addListener(new ChangeListener<EVolumeTypes>() {
+            @Override
+            public void changed(ObservableValue<? extends EVolumeTypes> observable, EVolumeTypes oldValue, EVolumeTypes newValue) {
+                changeParametersVisible(newValue);
+                changeParameterNames(newValue);
+            }
+        });
     }
 
-    public boolean IsCalculateButtonDisabled() {
-        return isCalculateButtonDisabled;
+    public BooleanProperty calculateDisableProperty() {
+        return isCalculateDisable;
+    }
+
+    public BooleanProperty getParam1VisibleProperty() {
+        return isParam1Visible;
+    }
+
+    public BooleanProperty getParam2VisibleProperty() {
+        return isParam2Visible;
+    }
+
+    public void setParam1VisibleProperty(boolean value) {
+        isParam1Visible.setValue(value);
+    }
+
+    public void setParam2VisibleProperty(boolean value) {
+        isParam2Visible.setValue(value);
+    }
+
+    public StringProperty resultVolumeProperty() {
+        return resultVolume;
+    }
+
+    public ObjectProperty<ObservableList<EVolumeTypes>> getVolumeTypeListItemsProperty() {
+        return volumeTypeListItems;
+    }
+
+    public void setSelectedVolumeItem(EVolumeTypes item) {
+        selectedVolumeItem.setValue(item);
+    }
+
+    public EVolumeTypes getSelectedVolumeItem() {
+        return selectedVolumeItem.getValue();
+    }
+
+    public StringProperty getParam1Name() {
+        return param1Name;
+    }
+    public StringProperty getParam2Name() {
+        return param2Name;
+    }
+
+    public void setParam1Name(final String value) {
+        param1Name.setValue(value);
+    }
+    public void setParam2Name(final String value) {
+        param2Name.setValue(value);
+    }
+
+    public ObjectProperty<EVolumeTypes> getSelectedItemProperty() { return selectedVolumeItem; }
+
+    private class SelectedVolumeItemChangeListener implements ChangeListener<EVolumeTypes> {
+
+        @Override
+        public void changed(ObservableValue<? extends EVolumeTypes> observable, EVolumeTypes oldValue, EVolumeTypes newValue) {
+            changeParametersVisible(newValue);
+            changeParameterNames(newValue);
+        }
+    }
+
+
+    private void changeParametersVisible(EVolumeTypes newValue) {
+        switch(newValue) {
+            case TETRAHEDRON:
+            case CUBE:
+            case SPHERE: {
+                setParam1VisibleProperty(true);
+                setParam2VisibleProperty(false);
+            } break;
+            case PYRAMID:
+            case CYLINDER:
+            case CONE: {
+                setParam1VisibleProperty(true);
+                setParam2VisibleProperty(true);
+            } break;
+            default: {
+                setParam1VisibleProperty(false);
+                setParam2VisibleProperty(false);
+            }
+        }
+    }
+
+    private void changeParameterNames(EVolumeTypes newValue) {
+        switch (newValue) {
+            case CUBE:
+            case TETRAHEDRON: {
+                setParam1Name("Edge");
+            } break;
+            case SPHERE: {
+                setParam1Name("Radius");
+            } break;
+            case PYRAMID: {
+                setParam1Name("Area");
+                setParam2Name("Height");
+            } break;
+            case CONE:
+            case CYLINDER: {
+                setParam1Name("Radius");
+                setParam2Name("Height");
+            } break;
+            default: {
+                setParam1Name("Param1");
+                setParam2Name("Param2");
+            }
+        }
+    }
+}
+
+enum EVolumeTypes {
+    SPHERE("Sphere"),
+    CUBE("Cube"),
+    CONE("Cone"),
+    CYLINDER("Cylinder"),
+    PYRAMID("Pyramid"),
+    TETRAHEDRON("Tetrahedron");
+
+    private final String name;
+    EVolumeTypes(final String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return name;
     }
 }
--- code/VolumeCalculator/ViewModel/src/test/java/ru/unn/agile/VolumeCalculator/viewModel/VolumeCalculatorViewModelTest.java
+++ code/VolumeCalculator/ViewModel/src/test/java/ru/unn/agile/VolumeCalculator/viewModel/VolumeCalculatorViewModelTest.java
@@ -1,23 +1,184 @@
 package ru.unn.agile.VolumeCalculator.viewModel;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.ObservableList;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by ponom on 24.11.2016.
- */
 public class VolumeCalculatorViewModelTest {
 
     private VolumeCalculatorViewModel viewModel;
     @Before
     public void setUp() {
+
         viewModel = new VolumeCalculatorViewModel();
     }
 
     @Test
     public void isCalculateButtonDisabledByDefault() {
-        assertEquals(true, viewModel.IsCalculateButtonDisabled());
+        assertEquals(true, viewModel.calculateDisableProperty().get());
     }
+
+    @Test
+    public void isParam1VisibleByDefault() {
+        assertEquals(false, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void isParam2VisibleByDefault() {
+        assertEquals(false, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void isResultEmptyByDefault() {
+        assertEquals("", viewModel.resultVolumeProperty().get());
+    }
+
+    @Test
+    public void isCorrectListBoxContent() {
+        ObjectProperty<ObservableList<EVolumeTypes>> items =  viewModel.getVolumeTypeListItemsProperty();
+
+        assertEquals(true, items.getValue().contains(EVolumeTypes.CONE));
+        assertEquals(true, items.getValue().contains(EVolumeTypes.CUBE));
+        assertEquals(true, items.getValue().contains(EVolumeTypes.CYLINDER));
+        assertEquals(true, items.getValue().contains(EVolumeTypes.PYRAMID));
+        assertEquals(true, items.getValue().contains(EVolumeTypes.SPHERE));
+        assertEquals(true, items.getValue().contains(EVolumeTypes.TETRAHEDRON));
+    }
+
+    @Test
+    public void whenGetSelectedValueItem() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.SPHERE);
+        assertEquals(EVolumeTypes.SPHERE, viewModel.getSelectedVolumeItem());
+    }
+
+    @Test
+    public void whenSphereChosenParameter1Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.SPHERE);
+        assertEquals(true, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void whenSphereChosenParameter2Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.SPHERE);
+        assertEquals(false, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void whenCubeChosenParameter1Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CUBE);
+        assertEquals(true, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void whenCubeChosenParameter2Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CUBE);
+        assertEquals(false, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void whenConeChosenParameter1Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CONE);
+        assertEquals(true, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void whenConeChosenParameter2Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CONE);
+        assertEquals(true, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void whenCylinderChosenParameter1Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CYLINDER);
+        assertEquals(true, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void whenCylinderChosenParameter2Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CYLINDER);
+        assertEquals(true, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void whenTetrahedronChosenParameter1Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.TETRAHEDRON);
+        assertEquals(true, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void whenTetrahedronChosenParameter2Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.TETRAHEDRON);
+        assertEquals(false, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void whenPyramidChosenParameter1Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.PYRAMID);
+        assertEquals(true, viewModel.getParam1VisibleProperty().get());
+    }
+
+    @Test
+    public void whenPyramidChosenParameter2Visible() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.PYRAMID);
+        assertEquals(true, viewModel.getParam2VisibleProperty().get());
+    }
+
+    @Test
+    public void whenTetrahedronChosenParameter1Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.TETRAHEDRON);
+        assertEquals("Edge", viewModel.getParam1Name().get());
+    }
+
+    @Test
+    public void whenCubeChosenParameter1Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CUBE);
+        assertEquals("Edge", viewModel.getParam1Name().get());
+    }
+
+    @Test
+    public void whenSphereChosenParameter1Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.SPHERE);
+        assertEquals("Radius", viewModel.getParam1Name().get());
+    }
+
+    @Test
+    public void whenPyramidChosenParameter1Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.PYRAMID);
+        assertEquals("Area", viewModel.getParam1Name().get());
+    }
+
+    @Test
+    public void whenPyramidChosenParameter2Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.PYRAMID);
+        assertEquals("Height", viewModel.getParam2Name().get());
+    }
+
+    @Test
+    public void whenCylinderChosenParameter1Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CYLINDER);
+        assertEquals("Radius", viewModel.getParam1Name().get());
+    }
+
+    @Test
+    public void whenCylinderChosenParameter2Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CYLINDER);
+        assertEquals("Height", viewModel.getParam2Name().get());
+    }
+
+    @Test
+    public void whenConeChosenParameter1Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CONE);
+        assertEquals("Radius", viewModel.getParam1Name().get());
+    }
+
+    @Test
+    public void whenConeChosenParameter2Name() {
+        viewModel.setSelectedVolumeItem(EVolumeTypes.CONE);
+        assertEquals("Height", viewModel.getParam2Name().get());
+    }
+
+
 }
